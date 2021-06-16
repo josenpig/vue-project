b00caab2d80c55ae5e29823fffdcc586d5c5f60d
LAPTOP-AMPEGKR5\lenovo <1104804222@qq.com>
2021-06-16 10:26:11 +0800

    产品

--- src/components/basicdata/AddProduct.vue
+++ src/components/basicdata/AddProduct.vue
@@ -2,87 +2,82 @@
 	<h3>新增产品</h3>
 	<div class="button">
 		<el-button size="small" @click="ToPro" class="button-no" type="primary">取消</el-button>
-		<el-button size="small" class="button-ok" type="primary">保存</el-button>
+		<el-button size="small" @click="ToPro" class="button-ok" type="primary">保存</el-button>
 	</div>
 	<el-tabs type="border-card">
 		<el-tab-pane label="用户管理">
-			<div class="tab">
-				<div>
-					<span class="must">*</span>
-					<span class="font">产品名称</span>
-					<el-input v-model="input" class="inpStyle"></el-input>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">产品编号</span>
-					<el-input v-model="input" class="inpStyle"></el-input>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">规格</span>
-					<el-input v-model="input" class="inpStyle" style="padding-left: 58px;"></el-input>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">销售价格</span>
-					<el-input-number v-model="form.ratio" :precision="2" :step="1" :min="0" style="margin-left: 30px;"/>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">采购价格</span>
-					<el-input-number v-model="form.ratio" :precision="2" :step="1" :min="0" style="margin-left: 30px;"/>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">单位</span>
-					<el-select v-model="form.unit" placeholder="请选择" style="padding-left: 58px;">
-						<el-option v-for="item in unitList" :key="item.value" :label="item.label" :value="item.value">
-						</el-option>
+			
+			<el-form :model="proForm">
+				<el-form-item label="* 产品编号  " :label-width="formLabelWidth">
+					<el-input v-model="proForm.productId" autocomplete="off" placeholder="(必填)" maxlength="20" show-word-limit></el-input>
+				</el-form-item>
+				<el-form-item label="* 产品名称 " :label-width="formLabelWidth">
+					<el-input v-model="proForm.productName" autocomplete="off" placeholder="(必填)" maxlength="20" show-word-limit></el-input>
+				</el-form-item>
+				<el-form-item label="* 产品成分 " :label-width="formLabelWidth">
+					<el-input v-model="proForm.ingredient" autocomplete="off" placeholder="(必填)" maxlength="20" show-word-limit></el-input>
+				</el-form-item>
+				<el-form-item label="* 产品规格 " :label-width="formLabelWidth">
+					<el-input v-model="proForm.productSpec" autocomplete="off" placeholder="(必填)" maxlength="20" show-word-limit></el-input>
+				</el-form-item>
+				<el-form-item label=" 克重 &nbsp;&nbsp;&nbsp;" :label-width="formLabelWidth">
+					<el-input v-model="proForm.gramHeavy" autocomplete="off" maxlength="20" show-word-limit></el-input>
+				</el-form-item>
+				<el-form-item label="* 产品单位 " :label-width="formLabelWidth">
+					<el-select v-model="proForm.unitId" placeholder="请选择产品单位  (必选)">
+						<el-option v-for="item in unit" :label="item.unitName" :value="item.unitId"></el-option>
 					</el-select>
-				</div>
-				<div>
-					<span class="must">*</span>
-					<span class="font">分类</span>
-					<el-select v-model="form.type" placeholder="请选择" style="padding-left: 58px;">
-						<el-option v-for="item in proType" :key="item.value" :label="item.label" :value="item.value">
-						</el-option>
+				</el-form-item>
+				<el-form-item label="* 产品分类 " :label-width="formLabelWidth">
+					<el-select v-model="proForm.productTypeId" placeholder="请选择产品单位  (必选)">
+						<el-option v-for="item in proType" :label="item.label" :value="item.id"></el-option>
 					</el-select>
-				</div>
-				<div>
-					<span class="font">&nbsp;&nbsp;备注</span>
-					<p></p>
-					<el-input class="bz" style="margin-top: -35px;padding-left: 92px;" type="textarea" :rows="5" v-model="textarea">
-					</el-input>
-				</div>
-				<div>
-					<span class="font">&nbsp;&nbsp;图片</span>
-					<div style="margin: -25px 90px">
-						<el-button size="small" type="primary">点击上传</el-button>
-					</div>
-				</div>
-			</div>
+				</el-form-item>
+				<el-form-item label="* 销售单价 " :label-width="formLabelWidth">
+					<el-input-number v-model="proForm.purchaseUnitPrice" :precision="0" :step="1" :min="0"></el-input-number>
+				</el-form-item>
+				<el-form-item label="* 采购单价 " :label-width="formLabelWidth">
+					<el-input-number v-model="proForm.purchaseMoney" :precision="0" :step="1" :min="0"></el-input-number>
+				</el-form-item>
+				<el-form-item label="产品描述" :label-width="formLabelWidth">
+					<el-input v-model="proForm.productDescribe" autocomplete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="  备注 &nbsp;&nbsp; " :label-width="formLabelWidth">
+					<el-input v-model="proForm.remarks" autocomplete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="  图片 &nbsp;&nbsp;" :label-width="formLabelWidth">
+					<el-button size="small" type="primary">点击上传</el-button>
+				</el-form-item>
+			</el-form>
+			
 		</el-tab-pane>
+
+		<!--////////初期库存////////////-->
 		<el-tab-pane label="初期库存">
-			<el-button type="primary" icon="el-icon-plus" style="margin-bottom: 10px;" @click="handleAddDetails">添加</el-button>
 			<!-- 表体内容 -->
 			<div>
-				<el-table :data="cusDate" style="width: 100%" @selection-change="handleSelectionChange" border stripe>
-					<el-table-column prop="customer" label="仓库" sortable width="406">
+				<el-table :data="stockData" style="width: 100%" @selection-change="handleSelectionChange" border stripe>
+					<el-table-column prop="depotName" label="仓库" sortable width="406">
 						<template #default="scope">
-							<el-select v-model="scope.row.customer" style="width:150px">
-								<el-option v-for="item in charge" :key="item.value" :label="item.label" :value="item.value">
+							<el-select v-model="stockData[scope.$index].depotName" style="width:150px">
+								<el-option v-for="item in depot" :key="item.depotName" :label="item.depotName" :value="item.depotName">
 								</el-option>
 							</el-select>
 						</template>
 					</el-table-column>
-					<el-table-column prop="number" label="初期库存" sortable width="300">
+					<el-table-column prop="openingNumber" label="初期库存" sortable width="300">
 						<template #default="scope">
-							<el-input v-model="scope.row.number"></el-input>
+							<el-input-number v-model="stockData[scope.$index].openingNumber" :precision="0" :step="1" :min="0"></el-input-number>
 						</template>
 					</el-table-column>
-					<el-table-column prop="date" label="操作" width="280">
+					<el-table-column label="操作" width="100" fixed>
 						<template #default="scope">
-							<el-button type="danger" size="small " icon="el-icon-delete" @click="handleDeleteDetails(scope.$index, scope.row)" circle></el-button>
+							<el-tooltip content="新增" placement="top">
+								<el-button size="mini" icon="el-icon-plus" @click="addrow()" type="primary" circle />
+							</el-tooltip>
+							<el-tooltip content="删除" placement="top">
+								<el-button size="mini" icon="el-icon-delete" @click="delrow(scope.$index, stockData)" type="primary" circle />
+							</el-tooltip>
 						</template>
 					</el-table-column>
 				</el-table>
@@ -95,48 +90,53 @@
 	export default {
 		data() {
 			return {
-				unitList: [{
-					value: '选项1',
-					label: '单位1'
-				}, {
-					value: '选项2',
-					label: '单位2'
-				}, {
-					value: '选项3',
-					label: '单位3'
-				}],
-				proType: [{
-					value: '选项1',
-					label: '分类1'
-				}, {
-					value: '选项2',
-					label: '分类2'
-				}, {
-					value: '选项3',
-					label: '分类3'
-				}],
-				charge: [{
-					value: '选项1',
-					label: '仓库1'
-				}, {
-					value: '选项2',
-					label: '仓库2'
-				}],
-				selcharge: '',
-				cusDate: [{
-					customer: '',
-					number: ''
-				}],
-				form: {
-					type: '',
-					unit: ''
+				//新增产品单
+				proForm: {
+					productId: '', //产品编号
+					productName: '', //产品名称
+					ingredient: '', //成份
+					productSpec: '', //规格
+					gramHeavy: '', //克重
+					unitId: '', //单位id： 连接单位表
+					productTypeId: '', //产品分类id： 连接分类表
+					purchaseUnitPrice: 0, //销售单价
+					purchaseMoney: 0, //采购单价
+					remarks: '', //备注
+					productDescribe: '', //产品描述
+					pictureId: 1, //图片id： 连接图片表
+					state: 1, //状态:（已启用：1)
+					userId: '', //创建人id: 连接用户表
 				},
 
+				//库存数据
+				stockData: [{
+					depotName: '', //仓库名称
+					openingNumber: 0 //期初数量
+				}],
+
+				//单位数据
+				unit: [{
+					unitId: 1,
+					unitName: '单位1'
+				}],
+
+				//产品分类数据
+				proType: [{
+					id: 1,
+					label: '分类1'
+				}],
+
+				//仓库数据
+				depot: [{
+					depotName: '仓库1'
+				}],
+
+				formLabelWidth: '120px',
 			}
 		},
 		computed: {},
 		methods: {
-			//取消添加
+			//返回
 			ToPro() {
 				this.$router.push("/Product")
 			},
@@ -151,8 +151,22 @@
 				this.cusDate.push(obj);
 			},
 			handleDeleteDetails(index, row) {
-				this.cusDate.splice(index,1);
-			}
+				this.cusDate.splice(index, 1);
+			},
+			//新增一行
+			addrow() {
+				this.stockData.push({
+					depotName: '', //仓库名称
+					openingNumber: 0 //期初数量
+				});
+			},
+			//移除一行
+			delrow(index, rows) {
+				console.log(this.stockData)
+				if (this.stockData.length > 1) {
+					rows.splice(index, 1); //删掉该行
+				}
+			},
 		}
 	}
 </script>
